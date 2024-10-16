7a4ac3a8f85fc538909b39822be951e9fa03eb50
Pau Botargues Julio <pbotargu@student.42barcelona.com>
2024-10-16 15:50:36 +0200

    ex04

---
+++ CPP-01/ex04/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+
+void    replacefile(std::ifstream &infile, const std::string &s1, const std::string &s2, std::ofstream &outfile)
+{
+    std::string str = "";
+    size_t i = 0;
+
+    while (std::getline(infile, str))
+    {
+        i = 0;
+        if (!s1.empty())
+        {
+            while ((i = str.find(s1, i)) != std::string::npos)
+            {
+                str = str.erase(i, s1.length());
+                str = str.insert(i, s2);
+                i += s2.length(); 
+            }
+        }
+        outfile << str << std::endl;
+    }
+}
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+    {
+        std::cout << "Error! Introduce [filename] [find] [replace]" << std::endl;
+        return (1);
+    }
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+    std::string ofilename = filename + ".replace";
+
+    std::ifstream infile(filename);
+    if (!infile.is_open())
+    {
+        std::cout << "Not possible to open the inputfile: " << filename << std::endl;
+        return (1);
+    } 
+    std::ofstream outfile(ofilename, std::ios::out | std::ios::trunc);
+    if (!outfile.is_open())
+    {
+        std::cout << "Not possible to open the outputfile: " << ofilename << std::endl;
+        return (1);
+    }
+    replacefile(infile, s1, s2, outfile);
+    if (!s1.empty())
+        std::cout << "Replace completed" << std::endl;
+    else
+        std::cout << "No changes" << std::endl;
+    infile.close();
+    outfile.close();
+    return (0);
+}
