2cb0c0f9afbf9f53dcdf8713c0e794cc2e9fb699
Pau Botargues Julio <pbotargu@student.42barcelona.com>
2024-11-18 14:06:32 +0100

    CPP-05 - presidential

--- CPP-05/ex02/PresidentialPardonForm.cpp
+++ CPP-05/ex02/PresidentialPardonForm.cpp
@@ -6,7 +6,52 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:39:20 by pbotargu          #+#    #+#             */
-/*   Updated: 2024/11/13 15:39:21 by pbotargu         ###   ########.fr       */
+/*   Updated: 2024/11/14 10:21:21 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PresidentialPardonForm.hpp"
+   
+PresidentialPardonForm::PresidentialPardonForm(): AForm("PresidentialPardonForm", 25, 5)
+{
+    std::cout << "PresidentialPardonForm deafult constructor" << std::endl;        
+    this->_target = "Undefined";
+}
+PresidentialPardonForm::PresidentialPardonForm(std::string target): AForm("PresidentialPardonForm", 25, 5)
+{
+    std::cout << "PresidentialPardonForm target constructor" << std::endl;
+    this->_target = target;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &other)
+{
+    std::cout << "PresidentialPardonForm copy constructor called" << std::endl;
+    (*this) = other;
+}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &other)
+{
+    std::cout << "PresidentialPardonForm copy assignment called" << std::endl;
+    AForm::operator=(other);
+    this->_target = other._target;
+    return (*this);
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+    std::cout << "PresidentialPardonForm destrucor called" << std::endl;
+}
+
+void    PresidentialPardonForm::executeAction()const
+{
+    std::cout << this->_target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
+        
+std::string PresidentialPardonForm::getTarget()
+{
+    return (this->_target);
+}
+void    PresidentialPardonForm::setTarget(std::string target)
+{
+    this->_target = target;
+}
--- CPP-05/ex02/PresidentialPardonForm.hpp
+++ CPP-05/ex02/PresidentialPardonForm.hpp
@@ -6,7 +6,27 @@
 /*   By: pbotargu <pbotargu@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:39:22 by pbotargu          #+#    #+#             */
-/*   Updated: 2024/11/13 15:39:23 by pbotargu         ###   ########.fr       */
+/*   Updated: 2024/11/14 10:17:42 by pbotargu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "AForm.hpp"
+#include <fstream>
+
+class PresidentialPardonForm : public AForm
+{
+    private:
+        std::string _target;
+    public:
+        PresidentialPardonForm();
+        PresidentialPardonForm(std::string target);
+        PresidentialPardonForm(const PresidentialPardonForm &other);
+        PresidentialPardonForm &operator=(const PresidentialPardonForm &other);
+        ~PresidentialPardonForm();
+
+        void    executeAction()const;
+        
+        std::string getTarget();
+        void    setTarget(std::string target);
+        
+};
