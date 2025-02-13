a7cf33a326feb6ce4be0fe1fc01bfb2fb381a7e7
Pau Botargues Julio <pbotargu@student.42barcelona.com>
2025-02-13 12:19:00 +0100

    CPP06<ex02>:Finished

---
+++ CPP-06/ex02/Base.cpp
@@ -0,0 +1,48 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base::~Base() {}
+
+Base *generate(void)
+{
+	int i = rand() % 3;
+
+	if (i == 0)
+		return (new A);
+	else if (i == 1)
+		return (new B);
+	else
+		return (new C);
+}
+
+void identify(Base* p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "A\n";
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B\n";
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C\n";
+}
+
+void identify(Base& p)
+{
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A\n";
+    } catch (std::bad_cast&) {}
+
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B\n";
+    } catch (std::bad_cast&) {}
+
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C\n";
+    } catch (std::bad_cast&) {}
+}
+
+
---
+++ CPP-06/ex02/main.cpp
@@ -0,0 +1,34 @@
+#include "Base.hpp"
+
+int main(){    
+    srand(time(0));
+	Base *base1 = generate();
+	Base *base2 = generate();
+	Base *base3 = generate();
+	Base *base4 = generate();
+
+	std::cout << std::endl << "Identificacion by pointer :" << std::endl << std::endl;
+	std::cout << "Base 1 : ";
+	identify(base1);
+	std::cout << "Base 2 : ";
+	identify(base2);
+	std::cout << "Base 3 : ";
+	identify(base3);
+	std::cout << "Base 4 : ";
+	identify(base4);
+	std::cout << std::endl << "Identification by reference :" << std::endl << std::endl;
+	std::cout << "Base 1 : ";
+	identify(*base1);
+	std::cout << "Base 2 : ";
+	identify(*base2);
+	std::cout << "Base 3 : ";
+	identify(*base3);
+	std::cout << "Base 4 : ";
+	identify(*base4);
+	delete base1;
+	delete base2;
+	delete base3;
+	delete base4;
+
+	return 0;
+}
