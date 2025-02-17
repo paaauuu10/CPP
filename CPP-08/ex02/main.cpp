528fb1ae1555a63f7d12997494d85510b2352c34
Pau Botargues Julio <pbotargu@student.42barcelona.com>
2025-02-17 19:28:12 +0100

    More tests

--- CPP-08/ex02/main.cpp
+++ CPP-08/ex02/main.cpp
@@ -9,15 +9,21 @@ int main()
     std::cout << mstack.top() << std::endl;
     mstack.pop();
     std::cout << mstack.size() << std::endl;
-    mstack.push(3); mstack.push(5); mstack.push(737); 
+    mstack.push(3); 
+    mstack.push(5); 
+    mstack.push(737); 
     //[...] 
     mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin(); MutantStack<int>::iterator ite = mstack.end();
+    MutantStack<int>::iterator it = mstack.begin(); 
+    MutantStack<int>::iterator ite = mstack.end();
     ++it;
     --it;
+    std::cout << std::endl << "----------------" << std::endl;
     while (it != ite) {
         std::cout << *it << std::endl;
-    ++it; }
-    std::stack<int> s(mstack); 
+        ++it; 
+    }
+    std::cout << "----------------" << std::endl;
+
     return 0;
 }
