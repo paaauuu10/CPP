0d020aa9612c7b2aab95d1e1581f3f2db35dee43
Pau Botargues Julio <pbotargu@student.42barcelona.com>
2025-02-21 13:26:13 +0100

    .hpp

---
+++ CPP-09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,26 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+
+class BitcoinExchange{
+    private:
+        std::map<std::string, double>   data;
+    
+    public: 
+        BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange &other);
+		BitcoinExchange &operator=(const BitcoinExchange &other);
+		~BitcoinExchange();
+
+        void    openFile(std::string name);
+        void printResult(const std::string& date, double n_btc, double rate);
+};
+
+
+
+#endif
