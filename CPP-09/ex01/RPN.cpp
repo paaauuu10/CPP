4ada48b39f25ca649a7808e3c34bc07e2e4710fa
Pau Botargues Julio <pbotargu@student.42barcelona.com>
2025-02-26 16:40:53 +0100

    polishOperation finished

--- CPP-09/ex01/RPN.cpp
+++ CPP-09/ex01/RPN.cpp
@@ -14,10 +14,46 @@ RPN::~RPN(){}
 
 void    RPN::polishOperation(std::string polish){
 
-    int a;
-    int b;
+    int first;
+    int second;
+    std::string operators = "+-*/";
 
     for (size_t i = 0; i < polish.length(); i++){
         if (isdigit(polish[i]))
+            this->stack.push(polish[i] - '0');
+        else if (polish[i] == ' ')
+            continue ; 
+        else if (operators.find(polish[i]) != std::string::npos){
+            if (stack.size() < 2){
+                std::cout << "Error" << std::endl << 
+                "Impossible to execute operand with only one number." << std::endl;
+                return ;
+            }
+            second = stack.top();
+            stack.pop();
+            first = stack.top();
+            stack.pop();
+            if (polish[i] == '+')
+                stack.push(first + second);
+            else if (polish[i] == '-')
+                stack.push(first - second);
+            else if (polish[i] == '*')
+                stack.push(first * second);
+            else if (polish[i] == '/')
+                stack.push(first / second);
+        }
+        else{
+            std::cout << "Error" << std::endl << "[" <<
+            polish[i] << "] : This is not a valid parameter" << std::endl;
+            return ;
+        }
     }
+    if (stack.size() == 1){
+        first = stack.top();
+        std::cout << first << std::endl;
+    } 
+    else
+        std::cout << "Error" << std::endl <<
+        "The polish operation was impossible to solve!" << std::endl;
+
 }
